b00b82bba2f8dea4cbd1707ee158418caeb1cb09
Yechao <liyechao@yechao.xyz>
2021-07-02 09:16:34 +0800

    Remove accessory view

--- ios/Classes/InAppWebView/InAppWebView.swift
+++ ios/Classes/InAppWebView/InAppWebView.swift
@@ -2941,11 +2941,11 @@ if(window.\(JAVASCRIPT_BRIDGE_NAME)[\(_callHandlerID)] != null) {
         print("InAppWebView - dealloc")
     }
     
-//    var accessoryView: UIView?
-//
-//    // https://stackoverflow.com/a/58001395/4637638
-//    public override var inputAccessoryView: UIView? {
-//        // remove/replace the default accessory view
-//        return accessoryView
-//    }
+   var accessoryView: UIView?
+
+   // https://stackoverflow.com/a/58001395/4637638
+   public override var inputAccessoryView: UIView? {
+       // remove/replace the default accessory view
+       return accessoryView
+   }
 }
